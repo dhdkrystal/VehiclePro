e3bdf2e4fde0e640e67e73affc90263c0a5a123b
dhd <609490912@qq.com>
2017-04-22 16:22:15 +0800

    bottomNavigation

--- app/src/main/java/com/xmucar/vehiclepro/MainActivity.java
+++ app/src/main/java/com/xmucar/vehiclepro/MainActivity.java
@@ -1,13 +1,48 @@
 package com.xmucar.vehiclepro;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_status:
+                    mTextMessage.setText(R.string.title_status);
+                    return true;
+                case R.id.navigation_market:
+                    mTextMessage.setText(R.string.title_market);
+                    return true;
+                case R.id.navigation_personal:
+                    mTextMessage.setText(R.string.title_personal);
+                    return true;
+            }
+            return false;
+        }
+
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
+
 }
