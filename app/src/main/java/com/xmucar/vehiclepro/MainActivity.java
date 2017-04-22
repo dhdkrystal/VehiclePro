8d05e2749d741428153573f3375067a9e61be306
Merxury <350699171@qq.com>
2017-04-22 17:18:07 +0800

    采用了第三方bottom bar实现，初步达到要求

--- app/src/main/java/com/xmucar/vehiclepro/MainActivity.java
+++ app/src/main/java/com/xmucar/vehiclepro/MainActivity.java
@@ -1,48 +1,57 @@
 package com.xmucar.vehiclepro;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
 import android.widget.TextView;
+
+import com.ashokvarma.bottomnavigation.BottomNavigationBar;
+import com.ashokvarma.bottomnavigation.BottomNavigationItem;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_status:
-                    mTextMessage.setText(R.string.title_status);
-                    return true;
-                case R.id.navigation_market:
-                    mTextMessage.setText(R.string.title_market);
-                    return true;
-                case R.id.navigation_personal:
-                    mTextMessage.setText(R.string.title_personal);
-                    return true;
-            }
-            return false;
-        }
-
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        initBottomBar();
+    }
+
+    private void initBottomBar() {
+        BottomNavigationBar bottomNavigationBar = (BottomNavigationBar) findViewById(R.id.bottom_navigation_bar);
+        bottomNavigationBar
+                .addItem(new BottomNavigationItem(R.drawable.ic_home_black_24dp, "主页"))
+                .addItem(new BottomNavigationItem(R.drawable.ic_status_24dp, "状态"))
+                .addItem(new BottomNavigationItem(R.drawable.ic_dashboard_black_24dp, "市场"))
+                .addItem(new BottomNavigationItem(R.drawable.ic_person_black_24dp, "我的"))
+                .setMode(BottomNavigationBar.MODE_FIXED)
+                .setActiveColor("#4d4d66")
+                .setBackgroundStyle(BottomNavigationBar.BACKGROUND_STYLE_RIPPLE)
+                .setFirstSelectedPosition(0)
+                .initialise();
+        bottomNavigationBar.setTabSelectedListener(new BottomNavigationBar.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(int position) {
+                switch (position) {
+                    case 0:
+                        mTextMessage.setText("主页");
+                        break;
+                    case 1:
+                        mTextMessage.setText("状态");
+                }
+            }
+
+            @Override
+            public void onTabUnselected(int position) {
+            }
+
+            @Override
+            public void onTabReselected(int position) {
+            }
+        });
     }
 
 }
