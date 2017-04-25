d7ae850bdc34a6fab4165a69b264f801aafcbd50
dhd <609490912@qq.com>
2017-04-25 21:47:17 +0800

    页面添加

--- app/src/main/java/com/xmucar/vehiclepro/Fragment/MarketFragment.java
+++ app/src/main/java/com/xmucar/vehiclepro/Fragment/MarketFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TabHost;
 
 import com.xmucar.vehiclepro.R;
 
@@ -15,6 +16,7 @@ import me.yokeyword.fragmentation.SupportFragment;
  */
 
 public class MarketFragment extends SupportFragment {
+    private TabHost tabHost;//声明TabHost组件的对象
     public static MarketFragment newInstance() {
         Bundle args = new Bundle();
         MarketFragment marketFragment = new MarketFragment();
@@ -24,8 +26,26 @@ public class MarketFragment extends SupportFragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.market_fragment, container, false);
+        // setContentView(R.layout.market_fragment); super.onCreate(savedInstanceState);
+
+
+        View view = inflater.inflate(R.layout.market_fragment, container, false);
+        tabHost = (TabHost) view.findViewById(android.R.id.tabhost);//获取tabHost对象
+        tabHost.setup();//初始化TabHost组件
+
+
+        inflater.inflate(R.layout.market_fragment_tab1, tabHost.getTabContentView());
+        inflater.inflate(R.layout.market_fragment_tab2, tabHost.getTabContentView());
+
+        tabHost.addTab(tabHost.newTabSpec("tab01")
+                .setIndicator("保修")
+                .setContent(R.id.linearLayout1));//添加第一个标签页
+        tabHost.addTab(tabHost.newTabSpec("tab02")
+                .setIndicator("市场")
+                .setContent(R.id.linearLayout2));//添加第二个标签页
+        return view;
     }
+
 
     @Override
     public void onAttach(Context context) {
