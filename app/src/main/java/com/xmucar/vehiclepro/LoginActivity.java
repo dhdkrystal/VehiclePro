d5c5728c917259220ef41a63bde34bf1a7a4c047
dhd <609490912@qq.com>
2017-04-26 22:49:49 +0800

    页面修改

--- app/src/main/java/com/xmucar/vehiclepro/LoginActivity.java
+++ app/src/main/java/com/xmucar/vehiclepro/LoginActivity.java
@@ -1,8 +1,33 @@
 package com.xmucar.vehiclepro;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+
+
 /**
  * Created by dhd on 2017/4/26.
  */
 
-public class LoginActivity {
+public class LoginActivity extends Activity {
+    private Button login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        login = (Button) findViewById(R.id.email_sign_in_button);
+        login.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            }
+        });
+
+
+    }
 }
